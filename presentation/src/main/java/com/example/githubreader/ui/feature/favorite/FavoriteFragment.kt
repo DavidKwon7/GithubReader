ce38777fa1b7bcedf62c3b9d2ecbe8b279437d22
DavidKwon7 <khjunejune10@gmail.com>
2022-08-23 23:58:09 +0900

    add alertDialog

--- presentation/src/main/java/com/example/githubreader/ui/feature/favorite/FavoriteFragment.kt
+++ presentation/src/main/java/com/example/githubreader/ui/feature/favorite/FavoriteFragment.kt
@@ -1,11 +1,13 @@
 package com.example.githubreader.ui.feature.favorite
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
 import com.example.common.base.BaseFragment
@@ -39,9 +41,32 @@ class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>() {
             (binding.rvFavorite.adapter as FavoriteAdapter).setItems(
                 it,
                 deleteItemClickListener = {
-                    Toast.makeText(getActivity(), "삭제", Toast.LENGTH_SHORT).show()
+                    alertDialog()
                 }
+
             )
         }
+    }
+
+    private fun alertDialog(
+        //afterAction: () -> Unit
+    ) {
+        AlertDialog.Builder(requireContext())
+            .setTitle("삭제")
+            .setMessage("삭제하시겠습니까?")
+            .setPositiveButton("네") { dialog, _ ->
+                // afterAction()
+                dialog.dismiss()
+                toastMessage("삭제되었습니다")
+            }
+            .setNegativeButton("아니요") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+    private fun toastMessage(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 }
